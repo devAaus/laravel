e951763fdb315f7601a665fbf74c7b30159646c9
Aayush <aausgh@gmail.com>
2025-01-17 13:59:56 +0545

    setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel </title>
+</head>
+<body>
+    <h1>Welcome to the new laravel blog app</h1>
+    <p>Click button to view list of blogs</p>
+
+    <a href="" class="btn">
+        Find Blogs
+    </a>
+</body>
+</html>

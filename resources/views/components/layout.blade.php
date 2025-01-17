102fb276d2ef2cfba361fe028bfd79b6d9762021
Aayush <aausgh@gmail.com>
2025-01-17 17:39:22 +0545

    tailwind css usage

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,13 +3,15 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Ninjas</title>
+
+   @vite('resources/css/app.css')
 </head>
 <body>
    <header>
       <nav>
          <h1>Ninjas</h1>
          <a href="/ninjas">All Ninjas</a>
-         <a href="/ninjas/create">Create</a>
+         <a href="/ninjas/create">Create New Ninja</a>
       </nav>
    </header>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel </title>
-</head>
-<body>
-    <h1>Welcome to the new laravel</h1>
-    <p>Click button to view list of ninjas</p>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Laravel </title>
 
-    <a href="/ninjas" class="btn">
-        Find Ninjas
-    </a>
+   @vite('resources/css/app.css')
+</head>
+<body class="text-center px-8 py-12">
+   <h1>Welcome to the new laravel app</h1>
+   <p>Click button to view list of ninjas</p>
+
+   <a href="/ninjas" class="btn mt-4 inline-block">
+      Find Ninjas
+   </a>
 </body>
 </html>

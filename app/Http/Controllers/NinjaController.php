3aff691f3bb7efe4ea87f5a8ee85c340e5a5ae46
Aayush <aausgh@gmail.com>
2025-01-17 20:37:51 +0545

    connecting ninjas and dojos with foreign key

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -8,18 +8,24 @@ use Illuminate\Http\Request;
 class NinjaController extends Controller
 {
     public function index() {
-        $ninjas = Ninja::orderBy('created_at', 'desc')->paginate(10);
+        // Get all ninjas with their dojos and order by creation date in descending order.
+        // route --> /ninjas/
+        $ninjas = Ninja::with('dojo')->orderBy('created_at', 'desc')->paginate(10);
 
         return view('ninjas.index', ["ninjas" => $ninjas]);
     }
 
     public function show($id) {
-        $ninja = Ninja::findOrFail($id);
+        // Get the ninja with the given ID and their dojo.
+        // route --> /ninjas/{id}
+        $ninja = Ninja::with('dojo')->findOrFail($id);
 
         return view('ninjas.show', ["ninja" => $ninja]);
     }
 
     public function create() {
+        // Show the form to create a new ninja.
+        // route --> /ninjas/create
         return view('ninjas.create');
     }
 
